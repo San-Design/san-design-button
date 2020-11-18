25f2846a9298f6f885ef5f7ca94b3c220df6b24c
Harika-BV <harikabhaskaram31@gmail.com>
2020-11-18 23:30:12 +0530

    Updated the button styles

--- san-design-button/src/App.js
+++ san-design-button/src/App.js
@@ -6,9 +6,82 @@ function App() {
 	
   return (
   	<div className="body">
-  		<h1>Buttons</h1>
+  		<h1>San Design Buttons</h1>
+  			<p>San Design Buttons are react components which are easy to customize in terms of size, shape and color</p>
 	  		<h2>Basic Buttons</h2>
-	  		<Button buttonSize="btn--wide" buttonColor="red" buttonVariant="btn--solid">Default</Button>
+	  		<Button buttonSize="btn--small" buttonColor="primary" buttonVariant="btn--solid">Primary</Button>
+	  		<Button buttonSize="btn--small" buttonColor="cyan" buttonVariant="btn--solid">Cyan</Button>
+	  		<Button buttonSize="btn--small" buttonColor="light-purple" buttonVariant="btn--solid">Light Purple</Button>
+	  		<Button buttonSize="btn--small" buttonColor="green" buttonVariant="btn--solid">Green</Button>
+	  		<Button buttonSize="btn--small" buttonColor="blue" buttonVariant="btn--solid">Blue</Button>
+	  		<Button buttonSize="btn--small" buttonColor="yellow" buttonVariant="btn--solid">Yellow</Button>
+	  		<Button buttonSize="btn--small" buttonColor="dark" buttonVariant="btn--solid">Dark</Button>
+	  		<Button buttonSize="btn--small" buttonColor="pink" buttonVariant="btn--solid">Pink</Button>
+	  		<Button buttonSize="btn--small" buttonColor="purple" buttonVariant="btn--solid">Purple</Button>
+	  		<Button buttonSize="btn--small" buttonColor="deep-purple" buttonVariant="btn--solid">Deep Purple</Button>
+	  		<Button buttonSize="btn--small" buttonColor="deep-orange" buttonVariant="btn--solid">Deep Orange</Button>
+	  		<Button buttonSize="btn--small" buttonColor="indigo" buttonVariant="btn--solid">Indigo</Button>
+	  		<Button buttonSize="btn--small" buttonColor="orange" buttonVariant="btn--solid">Orange</Button>
+	  		<Button buttonSize="btn--small" buttonColor="dark-green" buttonVariant="btn--solid">Dark Green</Button>
+	  		<Button buttonSize="btn--small" buttonColor="light-blue" buttonVariant="btn--solid">Light Blue</Button>
+	  		<Button buttonSize="btn--small" buttonColor="light-green" buttonVariant="btn--solid">Light Green</Button>
+	  		
+	  		<h2>Basic Outline Buttons</h2>
+	  		<Button buttonSize="btn--small" buttonBorderColor="primary-border" buttonVariant="btn--outline">Primary</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="cyan-border" buttonVariant="btn--outline">Cyan</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="light-purple-border" buttonVariant="btn--outline">Light Purple</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="green-border" buttonVariant="btn--outline">Green</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="blue-border" buttonVariant="btn--outline">Blue</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="yellow-border" buttonVariant="btn--outline">Yellow</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="dark-border" buttonVariant="btn--outline">Dark</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="pink-border" buttonVariant="btn--outline">Pink</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="purple-border" buttonVariant="btn--outline">Purple</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="deep-purple-border" buttonVariant="btn--outline">Deep Purple</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="deep-orange-border" buttonVariant="btn--outline">Deep Orange</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="indigo-border" buttonVariant="btn--outline">Indigo</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="orange-border" buttonVariant="btn--outline">Orange</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="dark-green-border" buttonVariant="btn--outline">Dark Green</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="light-blue-border" buttonVariant="btn--outline">Light Blue</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="light-green-border" buttonVariant="btn--outline">Light Green</Button>
+	  		
+	  		<h2>Basic Rounded Buttons</h2>
+	  		<Button buttonSize="btn--small" buttonColor="primary" buttonVariant="btn--solidRounded">Primary</Button>
+	  		<Button buttonSize="btn--small" buttonColor="cyan" buttonVariant="btn--solidRounded">Cyan</Button>
+	  		<Button buttonSize="btn--small" buttonColor="light-purple" buttonVariant="btn--solidRounded">Light Purple</Button>
+	  		<Button buttonSize="btn--small" buttonColor="green" buttonVariant="btn--solidRounded">Green</Button>
+	  		<Button buttonSize="btn--small" buttonColor="blue" buttonVariant="btn--solidRounded">Blue</Button>
+	  		<Button buttonSize="btn--small" buttonColor="yellow" buttonVariant="btn--solidRounded">Yellow</Button>
+	  		<Button buttonSize="btn--small" buttonColor="dark" buttonVariant="btn--solidRounded">Dark</Button>
+	  		<Button buttonSize="btn--small" buttonColor="pink" buttonVariant="btn--solidRounded">Pink</Button>
+	  		<Button buttonSize="btn--small" buttonColor="purple" buttonVariant="btn--solidRounded">Purple</Button>
+	  		<Button buttonSize="btn--small" buttonColor="deep-purple" buttonVariant="btn--solidRounded">Deep Purple</Button>
+	  		<Button buttonSize="btn--small" buttonColor="deep-orange" buttonVariant="btn--solidRounded">Deep Orange</Button>
+	  		<Button buttonSize="btn--small" buttonColor="indigo" buttonVariant="btn--solidRounded">Indigo</Button>
+	  		<Button buttonSize="btn--small" buttonColor="orange" buttonVariant="btn--solidRounded">Orange</Button>
+	  		<Button buttonSize="btn--small" buttonColor="dark-green" buttonVariant="btn--solidRounded">Dark Green</Button>
+	  		<Button buttonSize="btn--small" buttonColor="light-blue" buttonVariant="btn--solidRounded">Light Blue</Button>
+	  		<Button buttonSize="btn--small" buttonColor="light-green" buttonVariant="btn--solidRounded">Light Green</Button>
+	  
+
+	  		<h2>Basic Rounded Outline Buttons</h2>
+	  		<Button buttonSize="btn--small" buttonBorderColor="primary-border" buttonVariant="btn--outlineRounded">Primary</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="cyan-border" buttonVariant="btn--outlineRounded">Cyan</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="light-purple-border" buttonVariant="btn--outlineRounded">Light Purple</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="green-border" buttonVariant="btn--outlineRounded">Green</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="blue-border" buttonVariant="btn--outlineRounded">Blue</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="yellow-border" buttonVariant="btn--outlineRounded">Yellow</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="dark-border" buttonVariant="btn--outlineRounded">Dark</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="pink-border" buttonVariant="btn--outlineRounded">Pink</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="purple-border" buttonVariant="btn--outlineRounded">Purple</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="deep-purple-border" buttonVariant="btn--outlineRounded">Deep Purple</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="deep-orange-border" buttonVariant="btn--outlineRounded">Deep Orange</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="indigo-border" buttonVariant="btn--outlineRounded">Indigo</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="orange-border" buttonVariant="btn--outlineRounded">Orange</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="dark-green-border" buttonVariant="btn--outlineRounded">Dark Green</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="light-blue-border" buttonVariant="btn--outlineRounded">Light Blue</Button>
+	  		<Button buttonSize="btn--small" buttonBorderColor="light-green-border" buttonVariant="btn--outlineRounded">Light Green</Button>
+	  	
+	  		<h2>Icon Buttons</h2>
 	  		
 	  		<IconButton buttonSize="btn--wide" buttonColor="red" buttonVariant="btn--solid" leftIcon="fa-facebook">Default</IconButton>
 	  		<IconButton buttonSize="btn--large" buttonColor="red" buttonVariant="btn--solid" leftIcon="fa-facebook">Default</IconButton>
